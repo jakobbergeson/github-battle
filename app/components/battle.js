4c744208378da657cb01ee52e9ba51b8c245f44f
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-06-07 16:09:09 -0700

    Added a file and component name results that will compare the two github profiles. Created state property and conditional logic to render results component.

--- app/components/battle.js
+++ app/components/battle.js
@@ -7,6 +7,7 @@ import {
   faTimesCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
+import Results from "./Results";
 
 function Instructions() {
   return (
@@ -139,6 +140,7 @@ export default class Battle extends React.Component {
     this.state = {
       playerOne: null,
       playerTwo: null,
+      battle: false,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
@@ -155,7 +157,11 @@ export default class Battle extends React.Component {
     });
   }
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { playerOne, playerTwo, battle } = this.state;
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
+    }
+
     return (
       <React.Fragment>
         <Instructions />
@@ -189,6 +195,14 @@ export default class Battle extends React.Component {
               />
             )}
           </div>
+          {playerOne && playerTwo && (
+            <button
+              className="btn dark-btn btn-space"
+              onClick={() => this.setState({ battle: true })}
+            >
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     );

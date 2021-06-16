697d137a39cc97159427f406036b48656e228154
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-06-16 13:57:46 -0700

    Added react router v4 to project to add routes.

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import Popular from "./components/popular";
 import Battle from "./components/battle";
 import { ThemeProvider } from "./contexts/theme";
 import Nav from "./components/nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -20,15 +21,17 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <ThemeProvider value={this.state}>
-        <div className={this.state.theme}>
-          <div className="container">
-            <Nav />
-            <Popular />
-            {/* <Battle /> */}
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              <Route exact path="/" component={Popular} />
+              <Route path="/battle" component={Battle} />
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </Router>
     );
   }
 }

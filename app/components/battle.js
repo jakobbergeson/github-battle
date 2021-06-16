3cb8b9b7e849192d39cd5b7b628483ec2a4072fe
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-06-16 15:30:43 -0700

    Added query stringto deliver data to results component.

--- app/components/battle.js
+++ app/components/battle.js
@@ -7,8 +7,8 @@ import {
   faTimesCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
-import Results from "./Results";
 import { ThemeConsumer } from "../contexts/theme";
+import { Link } from "react-router-dom";
 
 function Instructions() {
   return (
@@ -155,7 +155,6 @@ export default class Battle extends React.Component {
     this.state = {
       playerOne: null,
       playerTwo: null,
-      battle: false,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
@@ -172,22 +171,7 @@ export default class Battle extends React.Component {
     });
   }
   render() {
-    const { playerOne, playerTwo, battle } = this.state;
-    if (battle === true) {
-      return (
-        <Results
-          playerOne={playerOne}
-          playerTwo={playerTwo}
-          onReset={() =>
-            this.setState({
-              playerOne: null,
-              playerTwo: null,
-              battle: false,
-            })
-          }
-        />
-      );
-    }
+    const { playerOne, playerTwo } = this.state;
 
     return (
       <React.Fragment>
@@ -223,12 +207,15 @@ export default class Battle extends React.Component {
             )}
           </div>
           {playerOne && playerTwo && (
-            <button
+            <Link
               className="btn dark-btn btn-space"
-              onClick={() => this.setState({ battle: true })}
+              to={{
+                pathname: "/battle/results",
+                search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`,
+              }}
             >
               Battle
-            </button>
+            </Link>
           )}
         </div>
       </React.Fragment>
